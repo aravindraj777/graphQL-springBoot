5fe3016564bfd609cc41651e50acaa8ebd6a2bf3
Aravind <aravindmunna240@gmail.com>
2024-04-05 11:19:47 +0530

    Initial commit

---
+++ src/main/java/com/practise/graph/GraphApplication.java
@@ -0,0 +1,13 @@
+package com.practise.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/graph/GraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
